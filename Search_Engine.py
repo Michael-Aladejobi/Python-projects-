0b871b946d211c10ce0924d56aebb5d1040c446e
Michael-Aladejobi <94129364+Michael-Aladejobi@users.noreply.github.com>
2022-04-07 13:51:15 +0100

    Create Search_Engine.py

---
+++ Search_Engine.py
@@ -0,0 +1,41 @@
+"""
+You’re working on a search engine. Watch your back Google!
+
+
+
+The given code takes a text and a word as input and passes them to a function called search().
+
+
+
+The search() function should return "Word found" if the word is present in the text, or "Word not found", if it’s not.
+
+
+
+Sample Input
+
+"This is awesome"
+
+"awesome"
+
+
+
+Sample Output
+
+Word found"""
+
+#solution
+def search(text,word):
+
+    if word in text:
+
+        return "Word found"
+
+    elif word not in text:
+
+        return "Word not found"
+
+text = input()
+
+word = input()
+
+print(search(text, word))
